0d5123cebefad6bb99a0e2405c3c8cbd1c21d1b6
rhkim <rhkim@local>
2026-09-17 22:03:43 +0900

    Keep click reward clear of boost cards

--- App.tsx
+++ App.tsx
@@ -1441,7 +1441,7 @@ const s0 = StyleSheet.create({
   crumbThree: { top: "50%", left: -3, width: 6, height: 6 },
   gain: {
     position: "absolute",
-    top: -28,
+    top: -10,
     backgroundColor: "#fff8e9",
     borderRadius: 18,
     borderWidth: 2,
